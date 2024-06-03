1b743db122fbfdafc48f701601c89af26a497f33
nathanassis <nathanfapereira@gmail.com>
2024-06-03 20:29:08 -0300

    feat(user): add icon and improve style

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,4 +1,6 @@
 import Container from "../../components/container";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
 import Image from "../../components/image";
 import Navbar from "../../components/navbar";
 import Section from "../../components/section";
@@ -14,20 +16,26 @@ function User() {
                     <Container classes="container-info">
                         <p className="container-user-text">Seu email é nomeusuario@email.com</p>
                         <p className="container-user-text">Seu CPF é 123.456.789-00</p>
-                        <a href="#">Alterar Dados</a>
-                        <a href="#">Excluir Conta</a>
-                        <a href="/admin">Tela do admin</a>
+                        <a className="user-link" href="#">
+                            Alterar Dados
+                            <FontAwesomeIcon className="user-icon" icon={faPencil} />
+                        </a>
+                        <a className="user-link" href="#">
+                            Excluir Conta
+                            <FontAwesomeIcon className="user-icon" icon={faTrash} />
+                        </a>
+                        <a className="admin-link user-link" href="/admin">Tela do admin</a>
                     </Container>
                 </Container>
                 <Container classes="container-games">
                     <h2 className="container-games-title">Jogos adquiridos:</h2>
                     <Container classes="container-games-row">
                         <Container classes="container-games-item">
-                            <Image src="/images/game1.png" height="250" width="190" />
+                            <Image src="/images/game2.png" height="250" width="190" />
                             <p className="container-games-grade">100/100</p>
                         </Container>
                         <Container classes="container-games-item">
-                            <Image src="/images/game1.png" height="250" width="190" />
+                            <Image src="/images/game3.png" height="250" width="190" />
                             <p className="container-games-grade">95/100</p>
                         </Container>
                     </Container>
@@ -37,7 +45,7 @@ function User() {
                             <p className="container-games-grade">75/100</p>
                         </Container>
                         <Container classes="container-games-item">
-                            <Image src="/images/game1.png" height="250" width="190" />
+                            <Image src="/images/game4.png" height="250" width="190" />
                             <p className="container-games-grade">Adicionar avaliação</p>
                         </Container>
                     </Container>
