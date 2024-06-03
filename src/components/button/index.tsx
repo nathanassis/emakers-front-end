a553328c988e318e3f0f8d3779c62dddb9d0fa72
nathanassis <nathanfapereira@gmail.com>
2024-06-03 14:30:39 -0300

    feat(cart): add icon and improve style

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,16 +2,17 @@ import './index.css';
 
 interface ButtonProps {
     name: string,
+    classes?: string,
     type: "submit" | "reset" | "button" | undefined,
     children?: React.ReactNode,
     style?: React.CSSProperties,
 }
 
 function Button(props: ButtonProps) {
-    let { name, type, children, style } = props;
+    let { name, classes, type, children, style } = props;
     
     return (
-        <button type={type} name={name} style={style}>{children}</button>
+        <button className={classes} type={type} name={name} style={style}>{children}</button>
     );
 }
 
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,5 +1,7 @@
 import Button from "../../components/button";
 import Container from "../../components/container";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import Form from "../../components/form";
 import Image from "../../components/image";
 import Input from "../../components/input";
@@ -17,42 +19,46 @@ function Cart() {
                     <Container classes="container-cart-item">
                         <Container classes="container-cart-info invert-color">
                             <Container classes="container-cart-title invert-color">
-                                <p>Sponge Bob: Battle for BikiniBottom</p>
-                                <p>Plataforma: PC</p>
+                                <p className="cart-game-title">Sponge Bob: Battle for BikiniBottom</p>
+                                <p className="cart-game-platform">Plataforma: PC</p>
                             </Container>
-                            <p className="item-price">R$299,00</p>
+                            <p className="item-price">R$ 299,00</p>
                         </Container>
                         <Image src="/images/game1.png" width="190" height="200" style={{ objectFit: "contain" }} />
                     </Container>
                     <Container classes="container-cart-item">
                         <Container classes="container-cart-info invert-color">
                             <Container classes="container-cart-title invert-color">
-                                <p>Sponge Bob: Battle for BikiniBottom</p>
-                                <p>Plataforma: PC</p>
+                                <p className="cart-game-title">Sponge Bob: Battle for BikiniBottom</p>
+                                <p className="cart-game-platform">Plataforma: PC</p>
                             </Container>
-                            <p className="item-price">R$299,00</p>
+                            <p className="item-price">R$ 299,00</p>
                         </Container>
                         <Image src="/images/game1.png" width="190" height="200" style={{ objectFit: "contain" }} />
                     </Container>
                     <hr style={{ margin: "0 120px" }}></hr>
                     <Container classes="container-game-price invert-color">
-                        <p>R$598,00</p>
-                        <p>Total</p>
+                        <p className="item-price">R$ 598,00</p>
+                        <p className="item-price">Total</p>
                     </Container>
                 </Container>
                 <Container classes="container-payment">
                     <h1>Método de Pagamento</h1>
                     <Form>
-                        <Container >
-                            <label htmlFor="pix">PIX</label>
+                        <Container classes="container-payment-method">
+                            <Image src="/images/pix.png" height="60" width="60" />
+                            Pix
                             <Input classes="container-payment-radio" type="radio" name="payment_method" />
                         </Container>
-                        <hr style={{margin: "40px 0"}}></hr>
-                        <Container>
+                        <hr style={{ margin: "40px 0", width: "400px" }}></hr>
+                        <Container classes="container-terms-and-conditions">
                             <Input classes="container-payment-radio" type="radio" name="terms_and_conditions" />
                             <label htmlFor="terms_and_conditions">Eu aceito os termos e condições</label>
                         </Container>
-                        <Button type="submit" name="submit_btn">Finalizar Compra</Button>
+                        <Button classes="cart-button" type="submit" name="submit_btn">
+                            Finalizar Compra
+                            <FontAwesomeIcon icon={faCartShopping} />
+                        </Button>
                     </Form>
                 </Container>
             </Section>
