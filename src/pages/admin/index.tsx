ced42ec84e4f6fb58dc0736cd078ae80752fe137
nathanassis <nathanfapereira@gmail.com>
2024-06-03 22:13:59 -0300

    feat(admin): add icon and improve style

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -18,14 +18,14 @@ function Admin() {
                         <Input type="text" name="new-gender" placeholder="Gênero" />
                         <textarea name="new-desc" placeholder="Descrição"></textarea>
                         <Input type="file" name="new-image" />
-                        <Button type="submit" name="new-submit">Cadastrar</Button>
+                        <Button classes="admin-button" type="submit" name="new-submit">Cadastrar</Button>
                     </Form>
                 </Container>
                 <Container classes="container-admin">
                     <h1 className="container-admin-title">Remover Jogo</h1>
                     <Form>
-                        <Input type="text" name="remove-title" placeholder="Título do jogo ou ID" />
-                        <Button type="submit" name="remove-submit">Remover</Button>
+                        <Input classes="invert-color" type="text" name="remove-title" placeholder="Título do jogo ou ID" />
+                        <Button classes="admin-button invert-color" type="submit" name="remove-submit">Remover</Button>
                     </Form>
                 </Container>
                 <Container classes="container-admin invert-color">
@@ -35,7 +35,7 @@ function Admin() {
                         <Input type="text" name="edit-gender" placeholder="Gênero" />
                         <textarea name="edit-desc" placeholder="Descrição"></textarea>
                         <Input type="file" name="edit-image" />
-                        <Button type="submit" name="edit-submit">Modificar</Button>
+                        <Button classes="admin-button" type="submit" name="edit-submit">Modificar</Button>
                     </Form>
                 </Container>
             </Section>
